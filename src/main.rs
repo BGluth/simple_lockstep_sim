c6551e716892e0cbb33523026446131d4a8f9aae
BGluth <gluthb@gmail.com>
2021-01-15 02:46:05 -0700

    Cleanup and bugfixes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod arg_parsing;
 mod simulation;
 
-use crate::arg_parsing::ProgArgLogLevel;
+use crate::arg_parsing::{ProgArgLogLevel, ProgArgs};
+use crate::simulation::UPDATE_CYCLE_FREQ;
+use log::info;
 
 fn main() {
-    let prog_args = arg_parsing::parse_prog_args();
-    setup_env_logger_from_prog_args_log_level(&prog_args.log_level);
+    let p_args = arg_parsing::parse_prog_args();
+    setup_env_logger_from_prog_args_log_level(&p_args.log_level);
+    print_info_about_prog_args(&p_args);
 
-    let mut ss = simulation::init_state(&prog_args);
+    let mut ss = simulation::init_state(&p_args);
 
-    for event_num in 0..prog_args.num_events_to_proc {
+    for event_num in 0..p_args.num_events_to_proc {
         println!("\n\n---------- Event #{}... ----------", event_num);
         simulation::run_sim_until_next_event(&mut ss);
     }
+
+    println!(
+        "Simulation finished running for {} events!",
+        p_args.num_events_to_proc
+    );
 }
 
 fn setup_env_logger_from_prog_args_log_level(log_level: &ProgArgLogLevel) {
@@ -26,4 +34,16 @@ fn setup_env_logger_from_prog_args_log_level(log_level: &ProgArgLogLevel) {
 
     std::env::set_var("RUST_LOG", log_env_var_str);
     pretty_env_logger::init();
+}
+
+fn print_info_about_prog_args(p_args: &ProgArgs) {
+    let lockstep_buffer_ms = p_args.l_buf_size * UPDATE_CYCLE_FREQ;
+
+    info!(
+        "Lockstep buffer size: {} frames ({}ms)",
+        p_args.l_buf_size, lockstep_buffer_ms
+    );
+    info!("Latency mean: {}ms", p_args.lat_mean);
+    info!("Latency std: {}ms", p_args.lat_std);
+    info!("Simulation length: {} events", p_args.num_events_to_proc);
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,14 +1,17 @@
 use crate::arg_parsing::ProgArgs;
 
+use log::info;
 use std::cmp::Ordering;
+use std::cmp::Reverse;
 use std::collections::binary_heap::BinaryHeap;
+use std::fmt;
 
 use rand_distr::{Distribution, Normal};
 
 type ClientID = usize;
 
 const FPS: usize = 60;
-const UPDATE_CYCLE_FREQ: usize = 1000 / FPS;
+pub const UPDATE_CYCLE_FREQ: usize = 1000 / FPS;
 
 pub struct SimState {
     ws: WorldState,
@@ -17,8 +20,19 @@ pub struct SimState {
 
 struct WorldState {
     ms_elapsed: usize,
-    upcoming_events: BinaryHeap<UpcomingEvent>,
+    upcoming_events: BinaryHeap<Reverse<UpcomingEvent>>,
     ls: LatencyState,
+}
+
+impl WorldState {
+    fn push_event(&mut self, event: UpcomingEvent) {
+        self.upcoming_events.push(Reverse(event));
+    }
+
+    fn get_next_event(&mut self) -> UpcomingEvent {
+        let reversed_event = self.upcoming_events.pop().unwrap();
+        reversed_event.0
+    }
 }
 
 struct LatencyState {
@@ -68,17 +82,28 @@ enum EventType {
     MessageArrival(Message),
 }
 
+impl fmt::Display for EventType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EventType::NextUpdateCycle(client_id) => {
+                write!(f, "Update cycle for client {}", *client_id)
+            }
+            EventType::MessageArrival(msg) => write!(
+                f,
+                "Message arrived for client {} from client {} for update cycle {}",
+                msg.dest_client, msg.sending_client, msg.target_update_cycle
+            ),
+        }
+    }
+}
+
 struct Client {
     client_id: ClientID,
     curr_update_cycle: usize,
     lockstep_buffer_size: usize,
     update_cycles_waiting_on: Vec<WaitingClientUpdateCycle>,
     next_update_cycle_that_we_are_waiting_on: usize,
-}
-
-struct WaitingClientUpdateCycle {
-    from_client: ClientID,
-    update_cycle: usize,
+    is_stalled: bool,
 }
 
 impl Client {
@@ -89,13 +114,14 @@ impl Client {
             lockstep_buffer_size: l_buf_size,
             update_cycles_waiting_on: Vec::new(),
             next_update_cycle_that_we_are_waiting_on: 0,
+            is_stalled: false,
         }
     }
 
     fn add_cycle_to_waiting_on(&mut self, update_cycle: usize, num_clients: usize) {
         let our_client_id = self.client_id;
 
-        for client_id in (0..num_clients).filter(|id| *id != our_client_id) {
+        for client_id in get_client_ids_that_are_not_ours(num_clients, our_client_id) {
             self.update_cycles_waiting_on
                 .push(WaitingClientUpdateCycle {
                     from_client: client_id,
@@ -103,6 +129,11 @@ impl Client {
                 });
         }
     }
+}
+
+struct WaitingClientUpdateCycle {
+    from_client: ClientID,
+    update_cycle: usize,
 }
 
 struct Message {
@@ -135,13 +166,19 @@ pub fn init_state(p_args: &ProgArgs) -> SimState {
     let mut ss = SimState { ws, clients };
 
     create_initial_client_input_frames(&mut ss, p_args.l_buf_size);
+    create_initial_client_update_cycle_events(&mut ss);
 
     ss
 }
 
 pub fn run_sim_until_next_event(ss: &mut SimState) {
-    let next_event = ss.ws.upcoming_events.pop().unwrap();
+    let next_event = ss.ws.get_next_event();
     ss.ws.ms_elapsed = next_event.event_trigger_time;
+
+    println!(
+        "Event: {} (sim time: {}ms)",
+        next_event.e_type, ss.ws.ms_elapsed
+    );
 
     match next_event.e_type {
         EventType::NextUpdateCycle(client_id) => handle_update_cycle_event(ss, client_id),
@@ -152,49 +189,92 @@ pub fn run_sim_until_next_event(ss: &mut SimState) {
 fn create_initial_client_input_frames(ss: &mut SimState, l_buf_size: usize) {
     // Get a sane start state by getting clients to send initial input state to each other.
     let num_input_frames_to_create = l_buf_size;
+    let num_clients = ss.clients.len();
 
+    info!("Creating initial input cycle update message to initialize the simulation...");
     for i in 0..num_input_frames_to_create {
         for sending_client_id in 0..ss.clients.len() {
-            for receiving_client_id in 0..ss.clients.len() {
+            for receiving_client_id in
+                get_client_ids_that_are_not_ours(ss.clients.len(), sending_client_id)
+            {
                 let target_frame = i;
                 let msg = Message::new(target_frame, sending_client_id, receiving_client_id);
                 send_msg(msg, &mut ss.ws);
             }
+
+            ss.clients[sending_client_id].add_cycle_to_waiting_on(i, num_clients);
         }
+    }
+}
+
+fn create_initial_client_update_cycle_events(ss: &mut SimState) {
+    for client_id in 0..ss.clients.len() {
+        add_event_for_next_update_cycle_start(&mut ss.ws, client_id, 0);
     }
 }
 
 fn send_msg(msg: Message, ws: &mut WorldState) {
     let delay_of_msg = ws.ls.gen_delay_of_msg();
 
+    info!(
+        "Sending update to client {} from client {} for cycle {} with a delay of {}ms...",
+        msg.dest_client, msg.sending_client, msg.target_update_cycle, delay_of_msg
+    );
+
     let upcoming_event = UpcomingEvent {
         event_trigger_time: ws.ms_elapsed + delay_of_msg,
         e_type: EventType::MessageArrival(msg),
     };
 
-    ws.upcoming_events.push(upcoming_event);
+    ws.push_event(upcoming_event);
 }
 
-fn add_event_for_next_update_cycle_start(ws: &mut WorldState, client_id: ClientID) {
+fn add_event_for_next_update_cycle_start(
+    ws: &mut WorldState,
+    client_id: ClientID,
+    time_of_next_update_cycle: usize,
+) {
+    info!(
+        "Scheduling next update cycle for client {} at {}ms...",
+        client_id, time_of_next_update_cycle
+    );
+
     let update_event = UpcomingEvent {
-        event_trigger_time: ws.ms_elapsed + UPDATE_CYCLE_FREQ,
+        event_trigger_time: time_of_next_update_cycle,
         e_type: EventType::NextUpdateCycle(client_id),
     };
 
-    ws.upcoming_events.push(update_event);
+    ws.push_event(update_event);
 }
 
 fn handle_update_cycle_event(ss: &mut SimState, client_id: ClientID) {
-    let client = &mut ss.clients[client_id];
+    let num_clients = ss.clients.len();
+    let curr_sim_time = ss.ws.ms_elapsed;
 
-    client.curr_update_cycle += 1;
+    let client = &mut ss.clients[client_id];
+    println!(
+        "Update cycle {} just completed for client {}",
+        client.curr_update_cycle, client_id
+    );
+
     if client.curr_update_cycle == client.next_update_cycle_that_we_are_waiting_on {
         // Stall!
+        client.is_stalled = true;
         println!("Client {} has stalled!", client_id);
         return;
     }
+    client.curr_update_cycle += 1;
 
-    add_event_for_next_update_cycle_start(&mut ss.ws, client_id);
+    let msg_target_cycle = client.curr_update_cycle + client.lockstep_buffer_size;
+    send_update_cycle_info_out_to_other_clients(
+        &mut ss.ws,
+        num_clients,
+        client_id,
+        msg_target_cycle,
+    );
+    client.add_cycle_to_waiting_on(msg_target_cycle, num_clients);
+
+    add_event_for_next_update_cycle_start(&mut ss.ws, client_id, curr_sim_time + UPDATE_CYCLE_FREQ);
 }
 
 fn handle_msg_received_event(ss: &mut SimState, msg: Message) {
@@ -207,14 +287,15 @@ fn send_update_cycle_info_out_to_other_clients(
     client_id: ClientID,
     target_update_cycle: usize,
 ) {
-    for other_client_id in (0..num_clients).filter(|id| *id != client_id) {
+    for other_client_id in get_client_ids_that_are_not_ours(num_clients, client_id) {
         let msg = Message::new(target_update_cycle, client_id, other_client_id);
         send_msg(msg, ws)
     }
 }
 
 fn process_recv_update_cycle_info_for_client(ss: &mut SimState, client_id: ClientID, msg: Message) {
-    let num_clients = ss.clients.len();
+    let curr_sim_time = ss.ws.ms_elapsed;
+
     let client = &mut ss.clients[client_id];
 
     let idx_of_waiting_msg = client
@@ -229,24 +310,29 @@ fn process_recv_update_cycle_info_for_client(ss: &mut SimState, client_id: Clien
         .update_cycles_waiting_on
         .swap_remove(idx_of_waiting_msg);
 
-    while client
-        .update_cycles_waiting_on
-        .iter()
-        .all(|x| x.update_cycle != client.next_update_cycle_that_we_are_waiting_on)
+    while !client.update_cycles_waiting_on.is_empty()
+        && client
+            .update_cycles_waiting_on
+            .iter()
+            .all(|x| x.update_cycle != client.next_update_cycle_that_we_are_waiting_on)
     {
-        client.next_update_cycle_that_we_are_waiting_on += 1;
-        let msg_target_cycle = client.curr_update_cycle + client.lockstep_buffer_size;
-        send_update_cycle_info_out_to_other_clients(
-            &mut ss.ws,
-            num_clients,
-            client_id,
-            msg_target_cycle,
-        );
-        client.add_cycle_to_waiting_on(msg_target_cycle, num_clients);
-
         println!(
             "Client {} just received all other pending client info for frame {}.",
-            client_id, client.curr_update_cycle
+            client_id, client.next_update_cycle_that_we_are_waiting_on
         );
+
+        if client.is_stalled {
+            add_event_for_next_update_cycle_start(&mut ss.ws, client_id, curr_sim_time); // Schedule another update cycle event immediately
+            client.is_stalled = false;
+        }
+
+        client.next_update_cycle_that_we_are_waiting_on += 1;
     }
+}
+
+fn get_client_ids_that_are_not_ours(
+    num_clients: usize,
+    our_client_id: ClientID,
+) -> impl Iterator<Item = usize> {
+    (0..num_clients).filter(move |id| *id != our_client_id)
 }
