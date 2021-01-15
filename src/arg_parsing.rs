93678c762f2218525c48f43d56ba0fb62d03d2cf
BGluth <gluthb@gmail.com>
2021-01-15 00:49:23 -0700

    Updated arg parsing
    - Now takes in lat_mean and lat_std.
    - Also added the number of events to process.

--- src/arg_parsing.rs
+++ src/arg_parsing.rs
@@ -1,19 +1,17 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
 const ARG_LOCKSTEP_BUF_SIZE_STR: &str = "lockstep_buffer_size";
-const ARG_LAT_SIM_TYPE: &str = "latency_sim_type";
+const ARG_LAT_MEAN: &str = "latency_mean";
+const ARG_LAT_STD: &str = "latency_std";
+const ARG_NUM_EVENTS_TO_PROC: &str = "num_events_to_proc";
 
 const DEFAULT_LOCKSTEP_BUFF_SIZE: &str = "3";
 
-pub enum LatencySimType {
-    CONSTANT = 0,
-}
-
 pub struct ProgArgs {
     pub l_buf_size: usize,
-    pub lat_type: LatencySimType,
     pub lat_mean: usize,
     pub lat_std: usize,
+    pub num_events_to_proc: usize,
 }
 
 pub fn parse_prog_args() -> ProgArgs {
@@ -30,31 +28,36 @@ pub fn parse_prog_args() -> ProgArgs {
                 .about("The lockstep buffer size"),
         )
         .arg(
-            Arg::new(ARG_LAT_SIM_TYPE)
-                .short('s')
-                .long("sim_type")
-                .value_name("LEVEL")
-                .case_insensitive(true)
-                .possible_values(&["c"])
-                .default_value("c")
-                .about("Sets the latency sim type"),
+            Arg::new(ARG_LAT_MEAN)
+                .short('m')
+                .long("lat_mean")
+                .default_value("50")
+                .about("The mean latency of all packets"),
+        )
+        .arg(
+            Arg::new(ARG_LAT_STD)
+                .short('d')
+                .long("lat_std")
+                .default_value("5")
+                .about("The standard deviation of the latency for msgs generated"),
+        )
+        .arg(
+            Arg::new(ARG_NUM_EVENTS_TO_PROC)
+                .short('n')
+                .long("num_events")
+                .default_value("100")
+                .about("The number of events to process in the simulation"),
         )
         .get_matches();
 
-    let l_buf_size = arg_matches
-        .value_of(ARG_LOCKSTEP_BUF_SIZE_STR)
-        .unwrap()
-        .parse()
-        .unwrap();
-    let lat_type = match arg_matches.value_of(ARG_LAT_SIM_TYPE).unwrap() {
-        "c" => LatencySimType::CONSTANT,
-        _ => panic!("Got an invalid latency sim type (should not be possible!)"),
-    };
-
     ProgArgs {
-        l_buf_size,
-        lat_type,
-        lat_mean: 50,
-        lat_std: 3,
+        l_buf_size: get_usize_val_of_prog_arg(&arg_matches, ARG_LOCKSTEP_BUF_SIZE_STR),
+        lat_mean: get_usize_val_of_prog_arg(&arg_matches, ARG_LAT_MEAN),
+        lat_std: get_usize_val_of_prog_arg(&arg_matches, ARG_LAT_STD),
+        num_events_to_proc: get_usize_val_of_prog_arg(&arg_matches, ARG_NUM_EVENTS_TO_PROC),
     }
+}
+
+fn get_usize_val_of_prog_arg(arg_matches: &ArgMatches, arg_str: &str) -> usize {
+    arg_matches.value_of(arg_str).unwrap().parse().unwrap()
 }
