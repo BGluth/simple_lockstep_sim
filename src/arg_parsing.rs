0015f8e1660f74c7a2ac7a0de9371522045db41d
BGluth <gluthb@gmail.com>
2021-01-15 01:41:50 -0700

    Setup pretty_env_logger

--- src/arg_parsing.rs
+++ src/arg_parsing.rs
@@ -4,14 +4,31 @@ const ARG_LOCKSTEP_BUF_SIZE_STR: &str = "lockstep_buffer_size";
 const ARG_LAT_MEAN: &str = "latency_mean";
 const ARG_LAT_STD: &str = "latency_std";
 const ARG_NUM_EVENTS_TO_PROC: &str = "num_events_to_proc";
+const ARG_NAME_STR_VERBOSITY: &str = "verbosity";
+
+const ARG_VAL_STR_VERB_TRACE: &str = "trace";
+const ARG_VAL_STR_VERB_DEBUG: &str = "debug";
+const ARG_STR_VAL_VERB_INFO: &str = "info";
+const ARG_STR_VAL_VERB_WARN: &str = "warn";
+const ARG_STR_VAL_VERB_ERROR: &str = "error";
 
 const DEFAULT_LOCKSTEP_BUFF_SIZE: &str = "3";
+
+#[derive(Debug)]
+pub enum ProgArgLogLevel {
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Err,
+}
 
 pub struct ProgArgs {
     pub l_buf_size: usize,
     pub lat_mean: usize,
     pub lat_std: usize,
     pub num_events_to_proc: usize,
+    pub log_level: ProgArgLogLevel,
 }
 
 pub fn parse_prog_args() -> ProgArgs {
@@ -48,13 +65,39 @@ pub fn parse_prog_args() -> ProgArgs {
                 .default_value("100")
                 .about("The number of events to process in the simulation"),
         )
+        .arg(Arg::new(ARG_NAME_STR_VERBOSITY)
+            .short('v')
+            .long(ARG_NAME_STR_VERBOSITY)
+            .value_name("LEVEL")
+            .possible_values(&[ARG_VAL_STR_VERB_TRACE, ARG_VAL_STR_VERB_DEBUG, ARG_STR_VAL_VERB_INFO, ARG_STR_VAL_VERB_WARN, ARG_STR_VAL_VERB_ERROR])
+            .case_insensitive(true)
+            .default_value(ARG_STR_VAL_VERB_INFO)
+            .about("Sets the verbosity level of program output info, where \"trace\" is the most verbose while \"error\" is the least"))
         .get_matches();
+
+    let log_level_str = arg_matches
+        .value_of(ARG_NAME_STR_VERBOSITY)
+        .unwrap()
+        .to_lowercase();
+
+    let log_level = match log_level_str.as_str() {
+        ARG_VAL_STR_VERB_TRACE => ProgArgLogLevel::Trace,
+        ARG_VAL_STR_VERB_DEBUG => ProgArgLogLevel::Debug,
+        ARG_STR_VAL_VERB_INFO => ProgArgLogLevel::Info,
+        ARG_STR_VAL_VERB_WARN => ProgArgLogLevel::Warn,
+        ARG_STR_VAL_VERB_ERROR => ProgArgLogLevel::Err,
+        _ => panic!(
+            "Somehow got a not valid verbosity log level ({}). This should not be possible!",
+            log_level_str
+        ),
+    };
 
     ProgArgs {
         l_buf_size: get_usize_val_of_prog_arg(&arg_matches, ARG_LOCKSTEP_BUF_SIZE_STR),
         lat_mean: get_usize_val_of_prog_arg(&arg_matches, ARG_LAT_MEAN),
         lat_std: get_usize_val_of_prog_arg(&arg_matches, ARG_LAT_STD),
         num_events_to_proc: get_usize_val_of_prog_arg(&arg_matches, ARG_NUM_EVENTS_TO_PROC),
+        log_level,
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 mod arg_parsing;
 mod simulation;
 
+use crate::arg_parsing::ProgArgLogLevel;
+
 fn main() {
     let prog_args = arg_parsing::parse_prog_args();
+    setup_env_logger_from_prog_args_log_level(&prog_args.log_level);
 
     let mut ss = simulation::init_state(&prog_args);
 
-    loop {
+    for event_num in 0..prog_args.num_events_to_proc {
+        println!("\n\n---------- Event #{}... ----------", event_num);
         simulation::run_sim_until_next_event(&mut ss);
     }
+}
+
+fn setup_env_logger_from_prog_args_log_level(log_level: &ProgArgLogLevel) {
+    let log_env_var_str = match log_level {
+        ProgArgLogLevel::Trace => "trace",
+        ProgArgLogLevel::Debug => "debug",
+        ProgArgLogLevel::Info => "info",
+        ProgArgLogLevel::Warn => "warn",
+        ProgArgLogLevel::Err => "error",
+    };
+
+    std::env::set_var("RUST_LOG", log_env_var_str);
+    pretty_env_logger::init();
 }
